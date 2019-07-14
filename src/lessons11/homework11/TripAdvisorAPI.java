d9c72e9bbb84168bad0fe029f3cab7cab0d5db56
Lenovo T540p <sonychkalapochka94@gmail.com>
2019-07-14 15:07:44 +0300

    lesson10

--- src/lessons11/homework11/TripAdvisorAPI.java
+++ src/lessons11/homework11/TripAdvisorAPI.java
@@ -13,24 +13,45 @@ public class TripAdvisorAPI implements API {
 
         int len = 0;
         for (Room room : rooms) {
-            if (room != null && persons > 0 && room.getPersons() > 0 && price == room.getPrice()
-                    && persons > (room.getPersons() - 1) && persons < (room.getPersons() + 1)
-                    && city == room.getCityName()
-                    && hotel == room.getHotelName()) {
-                len++;
+            if (room != null) {
+                if (persons > 0 && room.getPersons() > 0) {
+                    if (price == room.getPrice() && city == room.getCityName() && hotel == room.getHotelName()) {
+                        if (persons == room.getPersons()
+                                || persons == (room.getPersons() - 1)
+                                || persons == (room.getPersons() + 1)) {
+                            len++;
+                        }
+                    }
+                }
             }
         }
         Room[] roomPer = new Room[len];
         int i = 0;
         for (Room room : rooms) {
-            if (room != null && persons > 0 && room.getPersons() > 0 && price == room.getPrice()
-                    && persons > (room.getPersons() - 1) && persons < (room.getPersons() + 1)
-                    && city == room.getCityName()
-                    && hotel == room.getHotelName()) {
-                roomPer[i++] = room;
-            } else
-                return null;
+            if (room != null) {
+                if (persons > 0 && room.getPersons() > 0) {
+                    if (price == room.getPrice() && city == room.getCityName() && hotel == room.getHotelName()) {
+                        if (persons == room.getPersons()
+                                || persons == (room.getPersons() - 1)
+                                || persons == (room.getPersons() + 1)) {
+                            roomPer[i++] = room;
+                        }
+                    }
+                }
+            }
         }
+
+
+
+//        for (Room room : rooms) {
+//            if (room != null && persons > 0 && room.getPersons() > 0 && price == room.getPrice()
+//                    && persons > (room.getPersons() - 1) && persons < (room.getPersons() + 1)
+//                    && city == room.getCityName()
+//                    && hotel == room.getHotelName()) {
+//                roomPer[i++] = room;
+//            } else
+//                return null;
+//        }
         return roomPer;
     }
 
