fc65f2f69197719bc66324734c96d535f5e45e14
Lenovo T540p <sonychkalapochka94@gmail.com>
2019-08-24 21:46:28 +0300

    lesson19

--- src/lesson19/hw/Demo.java
+++ src/lesson19/hw/Demo.java
@@ -3,9 +3,9 @@ package lesson19.hw;
 public class Demo {
     public static void main(String[] args) {
 
-        File file1 = new File();
+        File file_1 = new File(1, "asdgsdgdfgsfdgsdfgdf", "doc", 12);
 
-        System.out.println(file1.toString());
+        System.out.println(file_1.toString());
 
     }
 }
