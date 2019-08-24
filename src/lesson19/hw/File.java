12e8ec724cfc642f94e9f32a51770b9d854bbe1b
Lenovo T540p <sonychkalapochka94@gmail.com>
2019-08-24 21:45:13 +0300

    lesson19

--- src/lesson19/hw/File.java
+++ src/lesson19/hw/File.java
@@ -6,7 +6,7 @@ public class File {
     private String format;
     private long size;
 
-    public File() {
+    public File(long id, String name, String format, long size) {
 
         try {
             if (name.length() <= 10){
