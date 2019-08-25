f697aaec8b778b6e1119e267e36930de21b36e34
Lenovo T540p <sonychkalapochka94@gmail.com>
2019-08-25 12:03:22 +0300

    lesson19

--- src/lesson19/hw/Storage.java
+++ src/lesson19/hw/Storage.java
@@ -79,11 +79,11 @@ public class Storage {
     private static boolean validation(Storage storage, File file)throws Exception{
                 if (file == null)
                     throw new Exception("Missing object"+ "storage = " + storage.id);
-                if (!validateFormat(storage, file) && file.getFormat()==null)
+                if (!validateFormat(storage, file))
                     throw new Exception("Format is not correct, id = " + file.getId() + " storage = " + storage.id);
                 if (!validateId(storage, file))
                     throw new Exception("Id is already in use, id = " + file.getId() + " storage = " + storage.id);
-                if (!validateFileName(file) && file.getName() == null)
+                if (!validateFileName(file))
                     throw new Exception("File name is too long, id = " + file.getId() + " storage = " + storage.id);
                 if (!validateStorageSize(storage, file))
                     throw new Exception("Not enough storage, id = " + file.getId() + " storage = " + storage.id);
