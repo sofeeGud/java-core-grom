1b1f42b5a44f5bb716549a1c4c9ddbb1aea3d705
Lenovo T540p <sonychkalapochka94@gmail.com>
2019-09-02 22:53:09 +0300

    lesson20 task2

--- src/lesson20/task2/TransactionDAO.java
+++ src/lesson20/task2/TransactionDAO.java
@@ -32,14 +32,8 @@ public class TransactionDAO {
 
     private void validate(Transaction transaction) throws BadRequestException {
 
-        if (transaction == null)
-            throw new BadRequestException("Transaction is null. Can't be saved");
-
-        if (transaction.getId() < 0
-                || transaction.getAmount() < 0
-                || transaction.getDateCreated() == null
-                || transaction.getType() == null)
-            throw new BadRequestException("Transaction is null " + transaction.getId() + " . Can't be saved");
+//        if (transaction == null)
+//            throw new BadRequestException("Transaction is null. Can't be saved");
 
         for (Transaction tr : transactions) {
             if (tr != null && tr.getId() == transaction.getId())
