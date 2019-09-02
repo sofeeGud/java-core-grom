9ffd4b73e9d3cb3069c74906e98dcaa6a958209d
Lenovo T540p <sonychkalapochka94@gmail.com>
2019-09-02 23:03:40 +0300

    lesson20 task2

--- src/lesson20/task2/TransactionDAO.java
+++ src/lesson20/task2/TransactionDAO.java
@@ -57,12 +57,11 @@ public class TransactionDAO {
         if (count == 0) throw new BadRequestException("Transaction already used " + transaction.getId() + ". Can't be saved");
 
 
-
-        if (sum + transaction.getAmount() > utils.getLimitTransactionsPerDayAmount()) {
+        if (sum + transaction.getAmount() >= utils.getLimitTransactionsPerDayAmount()) {
             throw new LimitExceeded("Transaction limit per day amount exceed " + transaction.getId() + ". Can't be saved");
         }
 
-        if (count > utils.getLimitTransactionsPerDayCount()) {
+        if (count >= utils.getLimitTransactionsPerDayCount()) {
             throw new LimitExceeded("Transaction limit per day count exceed " + transaction.getId() + ". Can't be saved");
         }
 
