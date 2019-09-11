bf29648f1eb490002da57fda20fdb4adcac88caa
Lenovo T540p <sonychkalapochka94@gmail.com>
2019-09-11 23:17:34 +0300

    lesson26 homework

--- src/lesson26/ArrayListTest.java
+++ src/lesson26/ArrayListTest.java
@@ -11,24 +11,49 @@ public class ArrayListTest {
         Order order4 = new Order(4, 598, "eur", "order4", "111");
         Order order5 = new Order(5, 200000, "eur", "order5", "111");
 
-        Order[] orders1 = {order1, order2, order3};
-        Order[] orders2 = {order4, order5};
 
-        ArrayList<Order> orders_1 = new ArrayList<>();
-        ArrayList<Order> orders_2 = new ArrayList<>();
+        try {
+            Order[] orders1 = {order1, order2, order3, order4,order5};
+            Order[] orders2 = {order4, order5};
+            Order[] orders3 = null;
 
-        System.out.println(useList(orders1, orders_1));
+            ArrayList<Order> orders_1 = new ArrayList<>();
+            ArrayList<Order> orders_2 = new ArrayList<>();
+            System.out.println(useList(orders1, orders_1, orders_2));
+
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
 
 
     }
 
-    private static ArrayList<Order> useList(Order[] order, ArrayList<Order> arrayList){
-        arrayList.add(order[0]);
-        arrayList.add(order[1]);
-        arrayList.add(order[2]);
-        arrayList.add(order[0]);
-        arrayList.add(order[1]);
+    private static ArrayList<Order> useList(Order[] orders, ArrayList<Order> arrayList1, ArrayList<Order> arrayList2) throws Exception{
+        if (orders == null || arrayList1 == null){
+            throw new Exception("null can't be used");}
+        else {
 
-        return arrayList;
+            arrayList1.add(orders[0]);
+            arrayList1.add(orders[1]);
+            arrayList1.add(orders[2]);
+            arrayList1.add(orders[3]);
+            arrayList1.add(1, orders[4]);
+            arrayList1.remove(orders[0]);
+            arrayList1.remove(0);
+            arrayList2.add(orders[0]);
+            arrayList2.add(orders[4]);
+            arrayList1.addAll(arrayList2);
+            arrayList1.subList(0,2);
+            arrayList1.set(2, orders[2]);
+            arrayList1.contains(orders[3]);
+
+            Object[] listArray = arrayList1.toArray();
+            for (int i = 0; i < listArray.length; i++) {
+                System.out.println(listArray[i]);
+            }
+            arrayList2.clear();
+
+            return arrayList1;
+        }
     }
 }
