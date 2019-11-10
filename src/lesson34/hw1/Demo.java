b4f948fb5c19a8da6632fe8e7e58fd5868ba8f54
Lenovo T540p <sonychkalapochka94@gmail.com>
2019-11-10 20:33:54 +0200

    lesson34 hw1

---
+++ src/lesson34/hw1/Demo.java
@@ -0,0 +1,9 @@
+package lesson34.hw1;
+
+public class Demo {
+    public static void main(String[] args) throws Exception {
+        Solution solution= new Solution();
+        solution.copyFileContent("C:/Users/Lenovo T540p/Desktop/test.txt", "C:/Users/Lenovo T540p/Desktop/test1.txt");
+
+    }
+}
---
+++ src/lesson34/hw1/Solution.java
@@ -0,0 +1,71 @@
+package lesson34.hw1;
+
+import java.io.*;
+
+public class Solution {
+
+    public void copyFileContent(String fileFromPath, String fileToPath) throws Exception {
+
+        validate(fileFromPath, fileToPath);
+
+        writeToFile(fileToPath, readFromFile(fileFromPath));
+        cleanFromFile(fileFromPath);
+    }
+
+    private StringBuffer readFromFile(String path) {
+        StringBuffer res = new StringBuffer();
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                res.append(line);
+                res.append("\r\n");
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File does not exist");
+        } catch (IOException e) {
+            System.err.println("Reading from file " + path + " failed");
+        }
+        return res;
+    }
+
+    private void writeToFile(String path, StringBuffer contentToWrite) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path, false))) {
+            bufferedWriter.append(contentToWrite);
+        } catch (IOException e) {
+            System.err.println("Can't write to file");
+        }
+    }
+
+    private void validate(String fileFromPath, String fileToPath) throws Exception {
+        File fileFrom = new File(fileFromPath);
+        File fileTo = new File(fileToPath);
+
+        if (!fileFrom.exists()) {
+            throw new FileNotFoundException("File " + fileFrom + " does not exist");
+        }
+
+        if (!fileTo.exists()) {
+            throw new FileNotFoundException("File " + fileTo + " does not exist");
+        }
+
+        if (!fileFrom.canRead()) {
+            throw new Exception("File " + fileFrom + " does not have permission to read");
+        }
+
+        if (!fileTo.canWrite()) {
+            throw new Exception("File " + fileTo + " does not have permission to written");
+        }
+        if (!fileFrom.canWrite()) {
+            throw new Exception("File " + fileFrom + " does not have permission to written");
+        }
+    }
+
+    private void cleanFromFile(String path) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path, false))) {
+            bufferedWriter.write("");
+        } catch (IOException e) {
+            System.err.println("Can't clear file by " + path);
+        }
+    }
+}
+
